67d89fc1adbe8cbbfbb4a60baeed12e2fbca36e5
Salman-Irfan <salmanirfan674@gmail.com>
2023-10-19 12:12:05 +0500

    comments table

---
+++ database/migrations/2023_10_19_070852_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->unsignedBigInteger('blog_id'); // Foreign key for the blog
+            $table->unsignedBigInteger('user_id'); // Foreign key for the user
+            $table->timestamps();
+
+            // Define foreign key constraints
+            $table->foreign('blog_id')->references('id')->on('blogs');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
