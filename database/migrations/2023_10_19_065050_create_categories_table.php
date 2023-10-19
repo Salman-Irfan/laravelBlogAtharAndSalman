e8f7a446fec221739fbce18cfb9c7e04f89eca78
Salman-Irfan <salmanirfan674@gmail.com>
2023-10-19 11:54:58 +0500

    category table

---
+++ database/migrations/2023_10_19_065050_create_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('isChild')->nullable(); // Define isChild as a foreign key
+            $table->timestamps();
+
+            // Define the foreign key constraint for isChild
+            $table->foreign('isChild')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
