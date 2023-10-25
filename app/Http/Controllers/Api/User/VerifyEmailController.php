deec762342c3eb05e8dd460716786b93a7ca61ef
Salman-Irfan <salmanirfan674@gmail.com>
2023-10-25 08:24:37 +0500

    verification email sent

--- app/Http/Controllers/Api/User/VerifyEmailController.php
+++ app/Http/Controllers/Api/User/VerifyEmailController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon; // for date time formatting
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\URL; // for dynamically generated URLs
 use Mail;
 use Illuminate\Support\Str; // for random string generator
@@ -15,12 +16,36 @@ class VerifyEmailController extends Controller
     public function verifyEmailController($email)
     {
         if (auth()->user()) {
-            $user = User::where("email", $email)->first();
+            $user = DB::table("users")->where("email", $email)->first();
             if ($user) {
+                $random = Str::random(40); // random string of 40 characters
+                $domain = URL::to('/'); // domain
+                $url = $domain .'/verify-mail/'. $random;
+
+                $data['url'] = $url;
                 $data['email'] = $email;
                 $data['title'] = 'Email Verification';
                 $data['body'] = 'Click on the link below to verify your Email';
-
+                // send verification link
+                Mail::send(
+                    'verifyMailViewFile', 
+                    [
+                        'data' => $data
+                    ],
+                    function ($message) use ($data) {
+                        $message
+                            ->to($data['email'])
+                            ->subject($data['title']);
+                    }
+                );
+                $user = User::find($user->id);
+                $user->remember_token = $random;
+                $user->save();
+                // sending response
+                return response()->json([
+                    'success'=>true,
+                    'message'=> 'Email sent successfully'
+                ]);
             } else {
                 return response()->json([
                     'success' => false,
@@ -33,6 +58,5 @@ class VerifyEmailController extends Controller
                 'message' => 'User is not Authenticated'
             ]);
         }
-
     }
 }
